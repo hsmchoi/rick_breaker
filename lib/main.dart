2725bb9f96db9f268bf65a04c7597b7562c5b85f
highshineme <86753850+hsmchoi@users.noreply.github.com>
2024-09-11 17:11:02 +0900

    main.dart modify

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
-import 'package:brick_breaker/src/brick_breaker.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+
+import 'src/brick_breaker.dart';
 
 void main() {
   final game = BrickBreaker();
   runApp(GameWidget(game: game));
 }
+
+//FlameGame => BrickBreaker()로 불러올수 있다. 
+// class BrickBreaker extends FlameGame {
+//   BrickBreaker()
+//       : super(
+//           camera: CameraComponent.withFixedResolution(
+//             width: gameWidth,
+//             height: gameHeight,
+//           ),
+//         );
+//   @override
+//   FutureOr<void> onLoad() async {
+//     super.onLoad();
+//     camera.viewport.anchor = Anchor.topLeft;
+//     world.add(PlayArea());
+//   }
+// }

6cb83bfe30c2b157f121785484f797fd96b21016
highshineme <86753850+hsmchoi@users.noreply.github.com>
2024-09-11 17:39:38 +0900

    Insert ball

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+import 'package:brick_breaker/src/components/ball.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:math' as math;
 import 'components/play_area.dart';
 import 'config.dart';
 
@@ -20,10 +21,21 @@ class BrickBreaker extends FlameGame {
     super.onLoad();
     camera.viewport.anchor = Anchor.topLeft;
     world.add(PlayArea());
+
+    final ball = Ball(
+        velocity: (Vector2(math.cos(math.pi / 4), math.sin(math.pi / 44))
+              ..normalize()) *
+            (gameHeight / 4),
+        radius: ballRadius,
+        position: Vector2(
+          gameWidth / 2,
+          gameHeight * 0.75,
+        ));
+    //공의 속도 velocity, 반지름 radius, 초기 위치를 설정한다.
+    //너비 /2 , 높이 * 0.75 = 중앙에서 아래로 25%
   }
 }
 
 //클래스 BrickBreaker
 //flame의 기본 로직
 //FlameGame 확장 (extends)
-
